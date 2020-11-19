478190bf116235aa8afd7ea9ae15a407a00e0d3a
Jmtri7 <Jmtri7@gmail.com>
2020-11-18 21:54:19 -0500

    Giant Tree / Fireflies
    new music

--- src/game/GameManager.java
+++ src/game/GameManager.java
@@ -20,7 +20,7 @@ public class GameManager extends AbstractGame {
 
 	public GameManager() {
 		BoardBuilder.LoadAssets();
-		gameBoard = BoardBuilder.BuildBoard("forest");
+		gameBoard = BoardBuilder.BuildBoard("tavern");
 	}
 
 	@Override
